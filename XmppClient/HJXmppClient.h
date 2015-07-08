c7fad5e60c1c73b683fc0771a59e541b406940ab
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-07-08 19:47:09 +0300

    Added xmpp protocols

---
+++ XmppClient/HJXmppClient.h
@@ -0,0 +1,13 @@
+//
+//  HJXmppClient.h
+//  HLJXmppWebSocket
+//
+//  Created by Oleksandr Dodatko on 7/8/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol HJXmppClient <NSObject>
+
+@end
---
+++ XmppClient/HJXmppClientDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HJXmppClientDelegate.h
+//  HLJXmppWebSocket
+//
+//  Created by Oleksandr Dodatko on 7/8/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol HJXmppClient;
+
+
+@protocol HJXmppClientDelegate <NSObject>
+
+- (void)xmppClent:(id<HJXmppClient>)sender
+didReceiveMessage:(id)message;
+
+@end
