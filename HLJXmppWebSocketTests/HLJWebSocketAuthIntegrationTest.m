2fb3b870ea6130db8070cf3030f483bfa74627c4
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-07-10 17:17:18 +0300

    Added initialisation code

--- HLJXmppWebSocketTests/HLJWebSocketAuthIntegrationTest.m
+++ HLJXmppWebSocketTests/HLJWebSocketAuthIntegrationTest.m
@@ -13,33 +13,55 @@
 #import <XmppFrameworkParsers/XmppFrameworkParsers.h>
 #import <HLJXmppWebSocket/HLJXmppWebSocket.h>
 
+#import "HJXmppClientImpl.h"
 
 @interface HLJWebSocketAuthIntegrationTest : XCTestCase
-
 @end
 
+
 @implementation HLJWebSocketAuthIntegrationTest
+{
+    HJXmppClientImpl* _sut      ;
+    SRWebSocket     * _webSocket;
+}
+
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    static NSString* const jid = @"070815_113113_qatest37_qatest37_general_question@conf.xmpp-dev.healthjoy.com/Qatest37 Qatest37 (id 11952)";
+
+    
+    // !!! needs manual updating
+    static NSString* const accessToken = @"dXNlcisxMTk1MkB4bXBwLWRldi5oZWFsdGhqb3kuY29tAHVzZXIrMTE5NTIAN3FKWjJIOFQ5V0FtWTNCU2M1Qkk3WVBOVXk3RG5E";
+    
+    NSURL* webSocketUrl = [NSURL URLWithString: @"wss://gohealth-dev.hjdev/ws-chat/"];
+    self->_webSocket = [[SRWebSocket alloc] initWithURL: webSocketUrl];
+    
+    XMPPParser* parser = [[XMPPParser alloc] initWithDelegate: nil
+                                                delegateQueue: NULL];
+    
+    self->_sut = [[HJXmppClientImpl alloc] initWithTransport: (id<HJTransportForXmpp>)self->_webSocket
+                                                  xmppParser: parser
+                                                        host: @"xmpp-dev.healthjoy.com"
+                                                 accessToken: accessToken
+                                               userJidString: jid];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    [self->_sut disconnect];
+    [self->_webSocket close];
+    
+    self->_sut = nil;
+    self->_webSocket = nil;
+    
     [super tearDown];
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
