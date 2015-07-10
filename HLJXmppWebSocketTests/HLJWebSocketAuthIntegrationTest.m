856d4e9a00a9cb821a9e9c0c8a0f764353f770a7
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-07-10 16:44:26 +0300

    added integration test stub

---
+++ HLJXmppWebSocketTests/HLJWebSocketAuthIntegrationTest.m
@@ -0,0 +1,40 @@
+//
+//  HLJWebSocketAuthIntegrationTest.m
+//  HLJXmppWebSocket
+//
+//  Created by Oleksandr Dodatko on 7/10/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface HLJWebSocketAuthIntegrationTest : XCTestCase
+
+@end
+
+@implementation HLJWebSocketAuthIntegrationTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
