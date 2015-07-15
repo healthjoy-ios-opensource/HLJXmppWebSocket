81da51b9e782023f6eceb989d1e70c5c8477d6b3
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-07-15 19:24:19 +0300

    [messages] Added a category for unit testing

---
+++ XmppClient/HJXmppClientImpl+UnitTest.h
@@ -0,0 +1,15 @@
+//
+//  HJXmppClientImpl+UnitTest.h
+//  HLJXmppWebSocket
+//
+//  Created by Oleksandr Dodatko on 7/15/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import "HJXmppClientImpl.h"
+
+@interface HJXmppClientImpl (UnitTest)
+
+@property (nonatomic, readonly) NSString* jidStringFromBind;
+
+@end
