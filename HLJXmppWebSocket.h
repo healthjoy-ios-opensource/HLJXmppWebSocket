697f6986808d3ce0363c0012f83ea7e1d1096454
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-07-22 16:48:08 +0300

    Exposed a missing header

--- HLJXmppWebSocket.h
+++ HLJXmppWebSocket.h
@@ -3,4 +3,7 @@
 #import <HLJXmppWebSocket/XmppClient/HJXmppClient.h>
 #import <HLJXmppWebSocket/XmppClient/HJXmppClientDelegate.h>
 
+#import <HLJXmppWebSocket/Attachments/HJXmppChatAttachment.h>
+
+
 
