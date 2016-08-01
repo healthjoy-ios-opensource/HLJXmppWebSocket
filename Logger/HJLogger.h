5905985a5c21ffa7208a6a6b9110c5778ca60171
Mark Prutskiy <makroo@yandex.ru>
2016-08-01 17:18:03 +0300

    HJLogger

---
+++ Logger/HJLogger.h
@@ -0,0 +1,13 @@
+//
+//  HJLogger.h
+//  HLJXmppWebSocket
+//
+//  Created by Mark Prutskiy on 8/1/16.
+//  Copyright © 2016 healthjoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol HJLogger <NSObject>
+
+@end
