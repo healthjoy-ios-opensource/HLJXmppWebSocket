c4944ede8894e4d597bc8772e22d2c4eaa3b95df
Mark Prutskiy <m.prutskiy@healthjoy.com>
2017-02-10 18:05:44 +0200

    fix attachment detect

--- Parser/HJXmppAttachmentsParser.m
+++ Parser/HJXmppAttachmentsParser.m
@@ -46,7 +46,10 @@
     NSString* fileSize     = [[castedRawAttachment attributeForName: @"file_size"] stringValue];
     NSString* rawImageSize = [[castedRawAttachment attributeForName: @"size"     ] stringValue];
     
-    BOOL isFileAttachment = ([[castedRawAttachment attributeForName: @"thumb_url"] stringValue]) ? NO : YES;
+    BOOL isFileAttachment =
+    ([fileName rangeOfString:@"jpg"].location == NSNotFound) &&
+    ([fileName rangeOfString:@"png"].location == NSNotFound) &&
+    ([fileName rangeOfString:@"gif"].location == NSNotFound);
     
     if(isFileAttachment)
     {
